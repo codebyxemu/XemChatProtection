045686959c151e33f19a211e84e1766feb9fc216
Magnus K <74688700+codebyxemu@users.noreply.github.com>
2023-02-16 19:21:49 +0100

    Configurable javadocs

--- src/main/java/me/xemu/xemchatprotection/utils/Configurable.java
+++ src/main/java/me/xemu/xemchatprotection/utils/Configurable.java
@@ -4,13 +4,21 @@ import me.xemu.xemchatprotection.XemChatProtection;
 
 import java.util.List;
 
+/**
+ * This class is responsible for the plugin operation in getting
+ * configuration paths
+ */
 public class Configurable {
 
 	private String path;
 	private ConfigFile type;
 	private Object returned;
 
-	// TODO: Java Docs
+	/**
+	 * Initiate the required values.
+	 * @param path The path to get from the specified "type" parameter.
+	 * @param type The configuration type to get it from. Located at: ${@link ConfigFile}
+	 */
 	public Configurable(String path, ConfigFile type) {
 		switch (type) {
 			case CONFIG_YML:
@@ -25,22 +33,42 @@ public class Configurable {
 		}
 	}
 
+	/**
+	 * Get path as string
+	 * @return String return value
+	 */
 	public String string() {
 		return (String) returned;
 	}
 
+	/**
+	 * Get path as integer
+	 * @return Integer return value
+	 */
 	public int integer() {
 		return (int) returned;
 	}
 
+	/**
+	 * Get path as boolean
+	 * @return Boolean return value
+	 */
 	public boolean bool() {
 		return (boolean) returned;
 	}
 
+	/**
+	 * Get object as List
+	 * @return List return value
+	 */
 	public List<Object> objects() {
 		return (List<Object>) returned;
 	}
 
+	/**
+	 * Get path as StringList
+	 * @return StringList return value
+	 */
 	public List<String> strings() {
 		return (List<String>) returned;
 	}
